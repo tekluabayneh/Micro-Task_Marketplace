2df4049b84fb234a2048a893962051a68df5ca7f
teklu abayneh <tekluabayneh@gmail.com>
2025-03-21 23:30:44 +0300

    Add new components directory for reusable UI elements

---
+++ client/src/components/LandingPageComponents/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HeroImage from "../../assets/1.png";
+const Home = () => {
+  return (
+    <section className="w-full h-screen">
+      <div
+        style={{
+          backgroundImage: `url(${HeroImage})`,
+          width: "100%",
+          minHeight: "500px",
+          maxHeight: "80%",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "#f0f0f0",
+          borderRadius: "1rem",
+        }}
+        className="flex p-5"
+      >
+        <h1 className="w-96 text-3xl text-white md:text-6xl lg:text-7xl font-semibold">
+          find the prefect freelancer <br /> or project on MicroWorks
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
