c6d9c1816942cbe1f2787849cfdf5276251c7900
teklu abayneh <tekluabayneh@gmail.com>
2025-03-21 23:30:18 +0300

    Add inline background image to Landing page with Tailwind styling

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Home from "../../components/LandingPageComponents/Home";
 
 const Landing = () => {
   return (
     <div>
       <h1>this is the landing page</h1>
+      <Home />
     </div>
   );
 };
