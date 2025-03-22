caff258889cb23b085f53650e3fbf90dd2a563bd
teklu abayneh <tekluabayneh@gmail.com>
2025-03-22 11:20:05 +0300

    Update App.jsx to use custom Tailwind color variables

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import Landing from "./pages/Landing/Landing";
-
+import Routers from "./pages/Routes/Routers";
 function App() {
   return (
     <main>
-      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 items-center">
-        <Landing />
+      <section className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 items-center">
+        <Routers />
       </section>
     </main>
   );
