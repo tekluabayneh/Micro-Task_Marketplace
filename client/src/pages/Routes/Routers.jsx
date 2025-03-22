9be37aada9e529ed31b121e74d50f112e04c7561
teklu abayneh <tekluabayneh@gmail.com>
2025-03-22 11:21:05 +0300

    Update Routers.jsx to reflect custom color styling in routes

--- client/src/pages/Routes/Routers.jsx
+++ client/src/pages/Routes/Routers.jsx
@@ -1,23 +1,27 @@
 // src/routers/Routers.jsx
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import Header from "../../components/Header/Header";
 
 // Lazy-loaded page components
-const Home = React.lazy(() => import("../pages/Home"));
-const About = React.lazy(() => import("../pages/About"));
-const Profile = React.lazy(() => import("../pages/Profile"));
-const Contact = React.lazy(() => import("../pages/Contact"));
-const NotFound = React.lazy(() => import("../pages/NotFound"));
+const LandingPage = React.lazy(() => import("../Landing/Landing"));
+const Profile = React.lazy(() => import("../Profile/Profile"));
+const Contact = React.lazy(() => import("../Contact/Contact"));
+const NotFound = React.lazy(() => import("../NotFound/NotFound"));
+// const About = React.lazy(() => import("../About/About"));
+const Home = React.lazy(() => import("../Home/Home"));
 
 const Routers = () => {
   return (
     <Suspense fallback={<div className="text-center">Loading...</div>}>
+        <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/protected/home" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
         <Route path="/profile/:id" element={<Profile />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
   );
