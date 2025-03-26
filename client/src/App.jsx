3bdff35b87498206c52240fd806c3d1c6a779ca9
teklu abayneh <tekluabayneh@gmail.com>
2025-03-27 01:47:03 +0300

    Update App component to integrate new routes and authentication logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Routers from "./pages/Routes/Routers";
 function App() {
   return (
     <main>
-      <section className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 items-center">
+      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 items-center">
         <Routers />
       </section>
     </main>
