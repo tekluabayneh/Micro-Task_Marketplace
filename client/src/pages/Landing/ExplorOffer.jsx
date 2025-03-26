cd54c8fcf0cb0cca0027629959b592b152375aa1
teklu abayneh <tekluabayneh@gmail.com>
2025-03-27 01:47:31 +0300

    Add Explore Offers section to Landing page for user engagement

--- client/src/pages/Landing/ExplorOffer.jsx
+++ client/src/pages/Landing/ExplorOffer.jsx
@@ -10,9 +10,12 @@ const ExplorOffer = () => {
         {ExploreOfferData.enterprise.tagline}
       </h3>
       <div className="w-full grid grid-cols-1 xs:grid-cols-2 mx-auto sm:grid-cols-2 md:grid-cols-4 gap-5 justify-center">
-        {ExploreOfferData.categories.map((item) => {
+        {ExploreOfferData.categories.map((item, i) => {
           return (
-            <div className="w-48 h-24 flex flex-col gap-5  rounded-sm cursor-pointer shadow bg-[var(--gray-95)] hover:bg-[var(--gray-70)] p-1">
+            <div
+              key={i}
+              className="w-48 h-24 flex flex-col gap-5  rounded-sm cursor-pointer shadow bg-[var(--gray-95)] hover:bg-[var(--gray-70)] p-1"
+            >
               <h5 className="text-start pl-5 text-wrap font-bold">
                 {item.name}
               </h5>
