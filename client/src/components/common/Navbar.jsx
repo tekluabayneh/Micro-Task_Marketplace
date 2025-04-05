6bd9820e84f543947397473267bf92156f6e307b
teklu abayneh <tekluabayneh@gmail.com>
2025-04-06 01:54:02 +0300

    Update: Modify Navbar.jsx in common components

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,25 +1,13 @@
 import React from "react";
-
+import DashBoardImg from "../../assets/map.svg";
 const Navbar = () => {
   return (
-    <nav className="bg-gray-50 px-6 py-4 h-40">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold text-blue-600">
-          Freelancer Dashboard
-        </h1>
-        <div className="flex items-center space-x-4">
-          <button className="text-gray-600 hover:text-blue-600">
-            Messages
-          </button>
-          <button className="text-gray-600 hover:text-blue-600">
-            Settings
-          </button>
-          <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
-            Logout
-          </button>
-        </div>
-      </div>
-    </nav>
+    <nav
+      className="bg-gray-50 px-6 py-4 h-40 custom-shadow  bg-center bg-cover bg-no-repeat  relative rounded-sm"
+      style={{
+        backgroundImage: `url(${DashBoardImg})`,
+      }}
+    ></nav>
   );
 };
 
