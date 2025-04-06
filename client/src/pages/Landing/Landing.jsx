e8a88bcf86ed42daa8e12b8c86f78e0819e9f454
teklu abayneh <tekluabayneh@gmail.com>
2025-04-06 20:03:09 +0300

    Add MicroSection and improve landing page structure

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,37 +1,17 @@
 import React from "react";
 import Home from "../../components/LandingPageComponents/Home";
 import ExplorOffer from "./ExplorOffer";
-import Hero2img from "../../assets/2.jpg";
 import Section from "./Section";
 import Section2 from "./Section2";
 import Footer from "../../components/LandingPageComponents/Footer";
+import MicroWork from "./MicroSection";
 
 const Landing = () => {
   return (
     <div className="mt-16">
       <Home />
 
-      <section className="w-full h-96 m-5 ">
-        <div className="mx-auto">
-          <h1 className="text-center font-black p-6 text-2xl">
-            Our Service Cater to the diverse <br /> need of freelancer and
-            client
-          </h1>
-
-          <div className="w-96  m-auto relative">
-            <img
-              style={{ borderRadius: "6rem" }}
-              className="w-full h-52 object-cover bg-cover"
-              src={Hero2img}
-              alt=""
-            />
-            <h1 className="absolute top-15 -right-9 w-28 h-16 flex justify-center  bg-green-600 rounded-2xl text-white font-bold items-center">
-              Quality
-            </h1>
-          </div>
-        </div>
-      </section>
-
+      <MicroWork />
       <ExplorOffer />
       <Section />
       <Section2 />
