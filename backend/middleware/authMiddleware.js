8f2d12fb4f0ae6f123d2f9d683b555231c255852
teklu abayneh <tekluabayneh@gmail.com>
2025-04-10 01:43:07 +0300

    Add custom middleware for auth and validation

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const RegisterMiddleware = (req, res, next) => {
+  const { firstName, lastName, email, password ,role} = req.body;
+
+  if (!firstName || !lastName || !email || !password || !role) {
+    return res.status(400).json({
+      message: "All fields  are required.",
+    });
+  }
+  next();
+};
+
+const LoginMiddleware = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "All fields  are required.",
+    });
+  }
+  next();
+};
+
+module.exports = { RegisterMiddleware, LoginMiddleware };
---
+++ backend/middleware/userexistCheck.js
@@ -0,0 +1,62 @@
+const db = require("../config/db");
+const bcrypt = require("bcryptjs");
+const CheckUserExistLogin = async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    let CheckQuery = "SELECT * FROM users  WHERE email = ?";
+    let [result] = await db.execute(CheckQuery, [email]);
+
+    if (result.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // check if password mach
+    let isMatch = await bcrypt.compare(password, result[0].password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "password or email is Invalid" });
+    }
+
+    // this let's me to use it in the route page using this name
+    req.userInfoFromDB = result;
+    next();
+  } catch (error) {
+    console.error("Error in CheckUserExist middleware:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const CheckUserExistRegister = async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    let CheckQuery = "SELECT * FROM users  WHERE email = ? ";
+    let [result] = await db.execute(CheckQuery, [email]);
+
+ 
+    if (result.length > 0) {
+      return res.status(401).json({ message: "user already exist" });
+    }
+
+    // this let's me to use it in the route page using this name
+    req.userInfoFromDB = result;
+    next();
+  } catch (error) {
+    console.error("Error in CheckUserExist middleware:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+module.exports = { CheckUserExistLogin, CheckUserExistRegister };
