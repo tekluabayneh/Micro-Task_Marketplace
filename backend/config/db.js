b0af67c6fa088b9f45c4d626764035f213429f4b
teklu abayneh <tekluabayneh@gmail.com>
2025-04-10 01:42:57 +0300

    Add DB and environment configuration files

---
+++ backend/config/db.js
@@ -0,0 +1,31 @@
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = db;
+const mysql = require("mysql2/promise");
+
+const db = mysql.createPool({
+  uri: "mysql://microWorkDB_gonetrack:b39c8547ee783e1f2355fff0b823979bd3264bd6@lvp4r.h.filess.io:3307/microWorkDB_gonetrack",
+});
+module.exports = db;
+
+async function testDBConnection() {
+  try {
+    let connection = await db.getConnection();
+    console.log("✅ Database connected successfully!");
+    connection.release();
+  } catch (error) {
+    console.error("❌ Failed to connect to the database:", error.message);
+  }
+}
+testDBConnection();
