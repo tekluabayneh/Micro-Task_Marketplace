76060032c0b38943814f923cd8736f810a88b915
teklu abayneh <tekluabayneh@gmail.com>
2025-04-10 22:54:50 +0300

    Fix Google authentication strategy setup

--- backend/controllers/GoogleAuth.js
+++ backend/controllers/GoogleAuth.js
@@ -1,20 +1,21 @@
 const db = require("../config/db");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 require("dotenv").config();
+
 const configureGoogleAuth = (passport) => {
   passport.use(
     new GoogleStrategy(
       {
-        clientID: process.env.GoogleAuthSecret,
-        clientSecret: process.env.GoogleAuthClientSecret,
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "http://localhost:3001/auth/google/callback",
       },
       (accessToken, refreshToken, profile, done) => {
         console.log(profile, refreshToken, accessToken);
-
         done(null, true);
       }
     )
   );
 };
+
 module.exports = configureGoogleAuth;
