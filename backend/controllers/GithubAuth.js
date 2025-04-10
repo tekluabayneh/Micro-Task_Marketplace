ec97185ac919beb8e95be49c68bddf8fde36aa3b
teklu abayneh <tekluabayneh@gmail.com>
2025-04-10 22:54:47 +0300

    Fix GitHub authentication strategy setup

--- backend/controllers/GithubAuth.js
+++ backend/controllers/GithubAuth.js
@@ -1,20 +1,21 @@
 const db = require("../config/db");
 const GitHubStrategy = require("passport-github2").Strategy;
 require("dotenv").config();
+
 const configureGitHubStrategy = (passport) => {
   passport.use(
     new GitHubStrategy(
       {
-        clientID: process.env.GithubAuthSecret,
-        clientSecret: process.env.GithubAuthClientSecret,
-        callbackURL: "http://localhost:3001/auth/google/callback",
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "http://localhost:3001/auth/github/callback",
       },
       (accessToken, refreshToken, profile, done) => {
         console.log(accessToken, refreshToken, profile);
-
         done(null, true);
       }
     )
   );
 };
+
 module.exports = configureGitHubStrategy;
