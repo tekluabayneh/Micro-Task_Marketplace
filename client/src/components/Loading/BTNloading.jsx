26c8b2d8986db22ec8c4c2c91371ebf2df8012ad
teklu abayneh <tekluabayneh@gmail.com>
2025-04-13 02:30:05 +0300

    Add new version of Loading component

---
+++ client/src/components/Loading/BTNloading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BTNloading = () => {
+  return (
+    <div class="dots-loading">
+      <span>.</span>
+      <span>.</span>
+      <span>.</span>
+    </div>
+  );
+};
+
+export default BTNloading;
