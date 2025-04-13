8abaffe458406dcfc54c019b3dec0d114d3ec9b5
teklu abayneh <tekluabayneh@gmail.com>
2025-04-14 00:28:05 +0300

    Fix: Updated GitHub authentication logic

--- backend/controllers/GithubAuth.js
+++ backend/controllers/GithubAuth.js
@@ -7,14 +7,25 @@ const configureGitHubStrategy = (passport) => {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "http://localhost:3001/auth/github/callback",
+        callbackURL: "http://localhost:5000/api/oauth/github/callback",
+        scope: ["user:email"],
       },
       (accessToken, refreshToken, profile, done) => {
         try {
-          console.log(accessToken, refreshToken, profile);
-          done(null, true);
+          console.log("profile", profile);
+          console.log("refreshToken", refreshToken);
+          console.log("accessToken", accessToken);
+
+          const user = {
+            id: profile.id,
+            username: profile.username,
+            photo: profile.photos?.[0]?.value || "",
+            email: profile.emails?.[0]?.value ?? null,
+          };
+
+          done(null, user);
         } catch (error) {
-          console.error("Error during Google OAuth:", error);
+          console.error("Error during Github OAuth:", error);
           return done(error, null);
         }
       }
