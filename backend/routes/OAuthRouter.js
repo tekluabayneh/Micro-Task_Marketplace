df6d1ef361a0f0cabf21ffd98ea1814c94938fab
teklu abayneh <tekluabayneh@gmail.com>
2025-04-13 22:52:35 +0300

    Adjust Google OAuth routes and callback handling

--- backend/routes/OAuthRouter.js
+++ backend/routes/OAuthRouter.js
@@ -1,30 +1,54 @@
 const passport = require("passport");
 const OauthRoute = require("express").Router();
 /// Google Oauth
+OauthRoute.get("/google", (req, res, next) => {
+  const { type } = req.query;
+
+  console.log("this is the user type", type);
+
+  // add this user type to the cookies and we can grab and check where to redirect the user
+  res.cookie("user_type", type, { httpOnly: true });
+
+  // and now continue login with google
+  passport.authenticate("google", { scope: ["profile", "email"] })(
+    req,
+    res,
+    next
+  );
+});
+
+// redirect the user if the user successfully registered
 OauthRoute.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // now ths is how we access the user type since we store it in cookies
+    let userType = req.cookies.user_type;
+    console.log("this is the user type", userType);
+    if (!req.user || !userType) {
+      return res.redirect("http://localhost:5173");
+    }
+
+    if (userType === "freelancer") {
+      return res.redirect("http://localhost:5173/FreelancerDashboard");
+    } else {
+      return res.redirect("http://localhost:5173/ClientDashboard");
+    }
+  }
+);
+
+/// github Oauth
+OauthRoute.get(
+  "/github",
+  passport.authenticate("github", { scope: ["profile", "email"] })
 );
 
 // redirect the user if the user successfully registered
 OauthRoute.get(
-  "/google/FreelancerDashboard",
-  passport.authenticate("google", { failureRedirect: "/" }, (req, res) => {
-    res.redirect("/FreelancerDashboard");
-  })
-);
-
-// redirect the user if the user successfully registered
-http: OauthRoute.get(
   "/github/callback",
   passport.authenticate("github", { failureRedirect: "/" }, (req, res) => {
     res.redirect("/dashboard");
   })
 );
 
-/// github Oauth
-OauthRoute.get(
-  "github",
-  passport.authenticate("github", { scope: ["profile", "email"] })
-);
 module.exports = OauthRoute;
