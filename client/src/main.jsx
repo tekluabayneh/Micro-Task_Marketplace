d84c48e2984b26df4bb7525f6680635880378504
teklu abayneh <tekluabayneh@gmail.com>
2025-04-15 00:57:50 +0300

    Updated main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+let queryclient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryclient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
