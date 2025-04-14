c3f97c181857845d5b29853582e8f2b88a9edb85
teklu abayneh <tekluabayneh@gmail.com>
2025-04-14 21:29:06 +0300

    Refactor OAuthRouter routes for clarity and security

--- backend/routes/OAuthRouter.js
+++ backend/routes/OAuthRouter.js
@@ -18,19 +18,26 @@ OauthRoute.get("/google", (req, res, next) => {
 // redirect the user if the user successfully registered
 OauthRoute.get(
   "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/" }),
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:5173",
+  }),
   (req, res) => {
-    // now ths is how we access the user type since we store it in cookies
-    let userType = req.cookies.user_type;
+    try {
+      console.log(req);
+      // now ths is how we access the user type since we store it in cookies
+      let userType = req.cookies.user_type;
 
-    if (!req.user || !userType) {
+      if (!req.user || !userType) {
+        return res.redirect("http://localhost:5173");
+      }
+
+      if (userType === "freelancer") {
+        return res.redirect("http://localhost:5173/FreelancerDashboard");
+      } else {
+        return res.redirect("http://localhost:5173/ClientDashboard");
+      }
+    } catch (error) {
       return res.redirect("http://localhost:5173");
-    }
-
-    if (userType === "freelancer") {
-      return res.redirect("http://localhost:5173/FreelancerDashboard");
-    } else {
-      return res.redirect("http://localhost:5173/ClientDashboard");
     }
   }
 );
@@ -55,22 +62,28 @@ OauthRoute.get(
 // redirect the user if the user successfully registered
 OauthRoute.get(
   "/github/callback",
-  passport.authenticate("github", { failureRedirect: "/" }),
+  passport.authenticate("github", {
+    failureRedirect: "http://localhost:5173",
+  }),
   (req, res) => {
-    // now ths is how we access the user type since we store it in cookies
-    let userType = req.cookies.user_type;
+    try {
+      // now ths is how we access the user type since we store it in cookies
+      let userType = req.cookies.user_type;
 
-    // check if there is no user or user type return the user to landing page
-    if (!req.user || !userType) {
+      // check if there is no user or user type return the user to landing page
+      if (!req.user || !userType) {
+        return res.redirect("http://localhost:5173");
+      }
+
+      // redirect the user based on the type
+
+      if (userType === "freelancer") {
+        return res.redirect("http://localhost:5173/FreelancerDashboard");
+      } else {
+        return res.redirect("http://localhost:5173/ClientDashboard");
+      }
+    } catch (error) {
       return res.redirect("http://localhost:5173");
-    }
-
-    // redirect the user based on the type
-
-    if (userType === "freelancer") {
-      return res.redirect("http://localhost:5173/FreelancerDashboard");
-    } else {
-      return res.redirect("http://localhost:5173/ClientDashboard");
     }
   }
 );
