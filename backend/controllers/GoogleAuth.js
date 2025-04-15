8106348773befd48fa620d52fb4cf257c6d2a4d0
teklu abayneh <tekluabayneh@gmail.com>
2025-04-15 22:37:14 +0300

    Update GoogleAuth.js logic or structure (if applicable)

--- backend/controllers/GoogleAuth.js
+++ backend/controllers/GoogleAuth.js
@@ -9,9 +9,11 @@ const configureGoogleAuth = async (passport) => {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "http://localhost:5000/api/oauth/google/callback",
+        passReqToCallback: true,
       },
-      async (accessToken, refreshToken, profile, done) => {
+      async (req, accessToken, refreshToken, profile, done) => {
         try {
+          console.log("this is the params is in here baby", profile);
           const user = {
             id: profile.id,
             firstName: profile.name.familyName,
@@ -19,36 +21,29 @@ const configureGoogleAuth = async (passport) => {
             email: profile.emails[0].value,
             photo: profile.photos[0].value,
           };
-          console.log("this is the profile thing in here", profile);
 
-          const userRole = profile._json.state
-            ? JSON.parse(profile._json.state).role
-            : null;
-          console.log("this is the profile thing in here", userRole);
+          // check if th user exist in the database
+          const checkQuery = "SELECT * FROM users WHERE email = ?";
+          let [result] = await db.execute(checkQuery, [user.email]);
 
-          // // check if th user exist in the database
-          // const checkQuery = "SELECT * FROM users WHERE email = ?";
-          // let [result] = await db.execute(checkQuery, [user.email]);
+          // if the user exist just login them
+          if (result.length > 0) {
+            done(null, user);
+            return;
+          }
 
-          // // if the user exist just login them
-          // if (result.length > 0) {
-          //   done(null, user);
-          //   return;
-          // }
+          // otherwise store the data in the database
+          const userRole = JSON.parse(req.query.state);
+          let data = [user.firstName, user.lastName, user.email, userRole.role];
+          const StoreData =
+            "INSERT INTO users (firstName, lastName, email, role) VALUES (?, ?, ?, ?)";
+          let [storeResult] = await db.execute(StoreData, data);
 
-          // // otherwise store the data in the database
-          // const userRole = req.cookies.user_type || req.query.type;
-          // let data = [user.firstName, user.lastName, user.email, userRole];
-          // const StoreData =
-          //   "INSERT INTO users (firstName, lastName, email, role) VALUES (?, ?, ?, ?)";
-          // let [storeResult] = await db.execute(StoreData, data);
-
-          // // is the string process is success done it
-          // if (storeResult.affectedRows) {
-          //   done(null, user);
-          //   return;
-          // }
-          done(null, user);
+          // is the string process is success done it
+          if (storeResult.affectedRows) {
+            done(null, user);
+            return;
+          }
           console.log(user);
         } catch (error) {
           console.error("Error during Google OAuth:", error);
