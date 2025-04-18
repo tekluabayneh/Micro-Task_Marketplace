d5fdd368a32c59141979637b689c1037c774bd53
teklu abayneh <tekluabayneh@gmail.com>
2025-04-19 01:58:55 +0300

    Add postJobmiddleware to verify user and job data before insert

---
+++ backend/middleware/postJobmiddleware.js
@@ -0,0 +1,33 @@
+const db = require("../config/db");
+
+const postJobmiddleware = async (req, res, next) => {
+  try {
+    let clientId = req.userid;
+    let { jobTitle, description, jobSize, budget, experience, skills } =
+      req.body;
+    let data = [
+      clientId,
+      jobTitle,
+      description,
+      jobSize,
+      budget,
+      experience,
+      skills,
+    ];
+    let insertQuery =
+      "INSERT INTO jobs(clientId, jobTitle, description, jobSize, budget, experience, skills)VALUES(?,?,?,?,?,?,?)";
+    let [insertResult] = await db.execute(insertQuery, data);
+
+    if (insertResult.affectedRows === 0) {
+      return res
+        .status(400)
+        .json({ message: "Error while inserting job data" });
+    }
+    next();
+  } catch (error) {
+    console.error("Insert Job Error:", error.message);
+    return res.status(500).json({ error: "Failed to post job" });
+  }
+};
+
+module.exports = postJobmiddleware;
