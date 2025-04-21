3d1077a458ccc778337b2dcc0c0724223efd77e5
teklu abayneh <tekluabayneh@gmail.com>
2025-04-21 19:10:34 +0300

    Update Modal component

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,18 +1,26 @@
+// components/DynamicPortal.jsx
 import React from "react";
+import ReactDOM from "react-dom";
 
-const Modal = ({ isOpen, children }) => {
-  return (
-    <div>
-      {isOpen ? (
-        <div>
-          <h1>this is the modal</h1>
-          <button onClick={!isOpen}>close modal</button>
-        </div>
-      ) : (
-        { children }
-      )}
-    </div>
+const DynamicPortal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+      <div className="bg-white p-4 rounded shadow-lg  relative">
+        <span
+          onClick={onClose}
+          className="material-symbols-outlined absolute top-2 right-6
+           border-1 cursor-pointer  border-green-600  bg-white shadow-2xl rounded-full text-green-600 text-sm"
+          style={{ fontSize: "32px" }}
+        >
+          close
+        </span>
+        {children}
+      </div>
+    </div>,
+    document.getElementById("portal-root")
   );
 };
 
-export default Modal;
+export default DynamicPortal;
