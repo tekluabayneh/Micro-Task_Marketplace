921275154e9e6add28c68ddfde0350f68ae34bf3
teklu abayneh <tekluabayneh@gmail.com>
2025-04-21 19:10:55 +0300

    Update Section2 component

--- client/src/pages/Landing/Section2.jsx
+++ client/src/pages/Landing/Section2.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import Hero3img from "../../assets/4.jpg";
 const Section2 = () => {
   return (
-    <section className="w-full h-[30rem] flex flex-col md:flex-row gap-0 m-1 rounded-lg overflow-hidden p-1 my-10">
+    <section
+      className="w-full h-[30rem] flex flex-col md:flex-row gap-0 m-1 rounded-lg overflow-hidden p-1 my-10"
+      id="Findgreatwork"
+    >
       <div className="w-auto md:w-3/6  rounded-l-lg overflow-hidden">
         <img
           className="w-full h-screen object-cover hidden md:block rounded-l-lg"
@@ -11,7 +14,7 @@ const Section2 = () => {
         />
       </div>
       <div className="w-full md:w-3/6   flex  gap-0 flex-col bg-[#086add] rounded-r-lg text-white p-5">
-        <h1 className="text-2xl"> For talent</h1>
+        <h1 className="text-2xl">For talent</h1>
         <h1 className="text-6xl font-semibold p-1">Find great work</h1>
         <p className="p-5 font-semibold">
           Connect with clients you’re passionate about collaborating with and
