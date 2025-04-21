5c06d9adc75bed94b09f20bef84dc9c0f7b9e048
teklu abayneh <tekluabayneh@gmail.com>
2025-04-21 19:10:31 +0300

    Update MyInfo component

--- client/src/components/Client/MyInfo.jsx
+++ client/src/components/Client/MyInfo.jsx
@@ -2,7 +2,6 @@ const MyInfo = () => {
   return (
     <div className="mb-4">
       <h2 className="text-2xl font-semibold text-gray-800">My Info</h2>
-      <p className="text-xs text-gray-600">This is a client account</p>
     </div>
   );
 };
