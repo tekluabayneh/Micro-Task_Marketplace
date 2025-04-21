f16bbf95254a9fb7b44761dc79617ef0c8071d16
teklu abayneh <tekluabayneh@gmail.com>
2025-04-21 19:10:18 +0300

    Update DashboardTabs component

--- client/src/components/Client/DashBoardComponent/DashboardTabs.jsx
+++ client/src/components/Client/DashBoardComponent/DashboardTabs.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import { cn } from "./utils";
 
-const DashboardTabs = ({ tab1, tab2, className = "as" }) => {
-  let [isActive, setisActive] = useState(false);
+const DashboardTabs = ({ tab1, tab2 }) => {
+  let [isActive, setisActive] = useState(true);
   return (
-    <div className={cn("w-full", className)}>
+    <div className="w-full">
       <div className="grid w-full grid-cols-2 mb-6 gap-2">
         <button
-          onClick={() => setisActive(!isActive)}
+          onClick={() => setisActive(true)}
           className={`${
-            !isActive ? "bg-green-300" : "bg-white"
+            isActive ? "bg-green-300" : "bg-white"
           } p-3 cursor-pointer rounded-sm custom-shadow font-medium`}
         >
           Jobs
         </button>
         <button
-          onClick={() => setisActive(!isActive)}
+          onClick={() => setisActive(false)}
           className={`${
-            isActive ? "bg-green-300" : "bg-white"
+            !isActive ? "bg-green-300" : "bg-white"
           } p-3 cursor-pointer rounded-sm custom-shadow font-medium`}
         >
           AI Recommendations
