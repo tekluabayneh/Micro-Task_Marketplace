9c34a3dc006ce856458e9d2a8c1e5645312707b0
teklu abayneh <tekluabayneh@gmail.com>
2025-04-22 18:24:38 +0300

    Modify app entry point and router setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import store from "./components/store/store.js";
 let queryclient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryclient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryclient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
