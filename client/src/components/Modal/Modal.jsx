4a464eba593ed5eba32ab29b67feb50d509a04f4
teklu abayneh <tekluabayneh@gmail.com>
2025-04-22 18:24:32 +0300

    Update modal behavior and style

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -6,7 +6,7 @@ const DynamicPortal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50    ">
       <div className="bg-white p-4 rounded shadow-lg  relative">
         <span
           onClick={onClose}
