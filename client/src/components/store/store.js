1b51dc132139b3698a3b1ee15ef7642cdcc912d0
teklu abayneh <tekluabayneh@gmail.com>
2025-04-22 18:24:58 +0300

    Add Redux store configuration

---
+++ client/src/components/store/store.js
@@ -0,0 +1,12 @@
+import clientSearchSlice from "../Slices/clientSearchSlice";
+import freelancerSearchSlice from "../Slices/freelancerSearchSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    freelancerSearchSlice: freelancerSearchSlice,
+    clientSearchSlice: clientSearchSlice,
+  },
+});
+
+export default store;
