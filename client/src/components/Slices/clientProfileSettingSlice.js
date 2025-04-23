d96619d94d0f8818f83c8f45b8d4e69c3f17acb6
teklu abayneh <tekluabayneh@gmail.com>
2025-04-23 23:33:24 +0300

    Create Redux slice for managing client profile state

---
+++ client/src/components/Slices/clientProfileSettingSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const clientProfileSettingSlice = createSlice({
+  name: "clientProfileSettingSlice",
+  initialState: { CL_slide: [] },
+  reducers: {
+    update: (state, action) => {
+      state.CL_slide = action.payload;
+      console.log("this is athe action", action.payload)
+    },
+  },
+});
+
+export const { update } = clientProfileSettingSlice.actions;
+export default clientProfileSettingSlice.reducer;
