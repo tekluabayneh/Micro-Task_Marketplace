a46003460cb7ca92c78d75118eefd8e924614e4d
teklu abayneh <tekluabayneh@gmail.com>
2025-04-23 23:32:55 +0300

    Register clientProfileSettingSlice in Redux store

--- client/src/components/store/store.js
+++ client/src/components/store/store.js
@@ -1,3 +1,4 @@
+import clientProfileSettingSlice from "../Slices/clientProfileSettingSlice";
 import clientSearchSlice from "../Slices/clientSearchSlice";
 import freelancerSearchSlice from "../Slices/freelancerSearchSlice";
 import { configureStore } from "@reduxjs/toolkit";
@@ -6,6 +7,7 @@ const store = configureStore({
   reducer: {
     freelancerSearchSlice: freelancerSearchSlice,
     clientSearchSlice: clientSearchSlice,
+    clientProfileSettingSlice: clientProfileSettingSlice,
   },
 });
 
