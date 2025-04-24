c393e38e23b59dccf0b7427e61d90ca14ceee0fb
teklu abayneh <tekluabayneh@gmail.com>
2025-04-24 23:47:02 +0300

    Refactor client profile page

--- client/src/pages/Profiles/ClientProfile.jsx
+++ client/src/pages/Profiles/ClientProfile.jsx
@@ -1,35 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CompanyDetails from "../../components/Client/CompanyDetail";
 import AccountSection from "../../components/Client/AccountSection";
 import MyInfo from "../../components/Client/MyInfo";
 import SettingsPage from "../SettingsPage/ClientSettingsPage ";
 import UpdatePorfile from "../../components/hooks/UpdatePorfile";
 import { useDispatch, useSelector } from "react-redux";
-
+import axios from "axios";
 
 const ClientProfile = () => {
-  const clientData = {
-    username: "T***yneh",
-    email: "t***a@gmail.com",
-    companyName: "Teklu Abayneh",
-    owner: "T***yneh",
-    phone: "",
-    vatId: "",
-    timeZone: "UTC+03:00 Baghdad, Kuwait, Nairobi, Riyadh",
-    address: "Ethiopia",
-  };
+  const [ClientProfileData, setClientProfileData] = useState(null);
 
   const dispatch = useDispatch();
   const profileData = useSelector(
     (state) => state.clientProfileSettingSlice.CL_slide
   );
 
+  useEffect(() => {
+    const FetchClientProfile = async () => {
+      try {
+        let email = localStorage.getItem("userEmail");
+        const response = await axios.get(
+          "http://localhost:5000/api/ClientProfileData",
+          {
+            params: { email },
+          }
+        );
+        // Make sure we got valid data before setting
+        if (response.data && response.data.length > 0) {
+          setClientProfileData(response.data[0]);
+        } else {
+          setClientProfileData(null);
+        }
+      } catch (error) {
+        console.error("❌ Error fetching client profile:", error);
+      }
+    };
+    FetchClientProfile();
+  }, []);
 
-  
+  // Prevent error on first render
+  if (!ClientProfileData) {
+    return <div className="mt-20 text-center">Loading profile...</div>;
+  }
   return (
     <main className="w-full mt-20 flex flex-col md:flex-row gap-3">
       <div className="w-full md:w-70">
-        <SettingsPage />
+        <SettingsPage
+          image={ClientProfileData?.image}
+          username={ClientProfileData?.owner_name}
+        />
         <UpdatePorfile />
       </div>
 
@@ -38,17 +57,23 @@ const ClientProfile = () => {
         <MyInfo />
 
         {/* Account Section */}
-        <AccountSection username={clientData.username} />
+        {ClientProfileData && (
+          <AccountSection
+            username={ClientProfileData?.owner_name}
+            companyname={ClientProfileData?.company_name}
+          />
+        )}
 
         {/* Company Details */}
-        <CompanyDetails
-          companyName={clientData.companyName}
-          owner={clientData.owner}
-          phone={clientData.phone}
-          vatId={clientData.vatId}
-          timeZone={clientData.timeZone}
-          address={clientData.address}
-        />
+        {ClientProfileData && (
+          <CompanyDetails
+            companyName={ClientProfileData.company_name}
+            owner={ClientProfileData.owner_name}
+            phone={ClientProfileData.phone}
+            industry={ClientProfileData.industry}
+            address={ClientProfileData.location}
+          />
+        )}
       </div>
     </main>
   );
