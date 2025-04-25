b2738c0807d72308e8269fa87c53e41adaa9e00e
teklu abayneh <tekluabayneh@gmail.com>
2025-04-25 18:28:20 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,9 @@
-import Index from "./components/Client/DashBoardComponent/Dashborad";
-import ClientHeader from "./pages/Header/ClientHeader";
-import FreelancerHeader from "./pages/Header/FreelacerHeader";
-import MobileNav from "./pages/Header/Header";
 import Routers from "./pages/Routes/Routers";
 function App() {
   return (
     <main>
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 items-center">
         <Routers />
-        {/* <ClientHeader /> */}
-        {/* <FreelancerHeader /> */}
-        {/* <MobileNav /> */}
       </section>
     </main>
   );
