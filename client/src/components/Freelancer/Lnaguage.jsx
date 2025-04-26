b2adcaf0880974a27b1768126d29dd430c522651
teklu abayneh <tekluabayneh@gmail.com>
2025-04-27 02:17:37 +0300

    Fix typo and improve Language component

--- client/src/components/Freelancer/Lnaguage.jsx
+++ client/src/components/Freelancer/Lnaguage.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import DynamicPortal from "../Modal/Modal";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
 
-const Language = () => {
+const Language = ({ data }) => {
+  const [isPortalOpen, setIsPortalOpen] = useState(false);
+  const dispatch = useDispatch();
+  let { language } = data[0];
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  let onSubmit = (data) => {
+    // dispatch(update(data));
+
+    reset();
+    setTimeout(() => {
+      setIsPortalOpen(false);
+    }, 2000);
+  };
   const profileData = {
     languages: [
       { name: "English", proficiency: "Fluent" },
@@ -12,6 +34,7 @@ const Language = () => {
       {/* Languages */}
       <div className="mb-6 relative">
         <span
+          onClick={() => setIsPortalOpen(true)}
           className="material-symbols-outlined absolute top-3 left-27
            border-1 cursor-pointer  border-green-600  bg-white shadow-2xl rounded-full text-green-600 text-sm"
           style={{ fontSize: "18px" }}
@@ -27,6 +50,68 @@ const Language = () => {
           ))}
         </ul>
       </div>
+
+      <DynamicPortal
+        isOpen={isPortalOpen}
+        onClose={() => setIsPortalOpen(false)}
+      >
+        {" "}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="space-y-9 w-[26rem] h-[25rem] p-4"
+        >
+          <div>
+            <label htmlFor="Language" className="block">
+              Language:
+            </label>
+            <input
+              {...register("Language", {
+                required: "Language is required",
+              })}
+              type="text"
+              className="glass w-full max-w-md px-5 py-3 rounded-md border border-white/20 custom-shadow bg-white/20 backdrop-blur-md text-lg text-gray-800  placeholder-gray-400 focus:outline-none
+               focus:ring-2 focus:ring-primary/40 transition"
+              placeholder="Enter New language"
+            />
+            {errors.Language && (
+              <p className="text-red-500">{errors.Language.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="Education" className="block">
+              Education:
+            </label>
+            <input
+              {...register("Education", {
+                required: "Education is required",
+              })}
+              type="text"
+              className="glass w-full max-w-md px-5 py-3 rounded-md border border-white/20 custom-shadow bg-white/20 backdrop-blur-md text-lg text-gray-800  placeholder-gray-400 focus:outline-none
+               focus:ring-2 focus:ring-primary/40 transition"
+              placeholder="Enter New Education"
+            />
+            {errors.Education && (
+              <p className="text-red-500">{errors.Education.message}</p>
+            )}
+          </div>
+          <div className="w-full flex gap-1 flex-col md:flex-row">
+            <button
+              type="submit"
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer w-full"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              onClick={() => setIsPortalOpen(false)}
+              className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer w-full mt-4"
+            >
+              Delete
+            </button>
+          </div>
+        </form>
+      </DynamicPortal>
     </div>
   );
 };
