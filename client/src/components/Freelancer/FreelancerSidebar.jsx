b47f4697e804532c89fe6bfb6fec2e4920e86a74
teklu abayneh <tekluabayneh@gmail.com>
2025-04-27 02:17:34 +0300

    Adjust FreelancerSidebar design and logic

--- client/src/components/Freelancer/FreelancerSidebar.jsx
+++ client/src/components/Freelancer/FreelancerSidebar.jsx
@@ -1,57 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Language from "./Lnaguage";
 import Education from "./Education";
+import DynamicPortal from "../Modal/Modal";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
-const FreelancerSidebar = () => {
-  const profileData = {
-    verifications: ["Military veteran"],
-    licenses: [],
+const FreelancerSidebar = ({ data }) => {
+  let { language, education, Licenses, isVerified } = data[0];
+  const [isPortalOpen, setIsPortalOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  let onSubmit = (data) => {
+    // dispatch(update(data));
+
+    reset();
+    setTimeout(() => {
+      setIsPortalOpen(false);
+    }, 2000);
   };
 
   return (
     <div className="md:w-96 max-w-full p-4 bg-white custom-shadow rounded-lg">
       {/* language */}
-      <Language />
+      <Language data={data} />
 
       {/* Verifications */}
       <div className="mb-6 relative">
-        <span
-          className="material-symbols-outlined absolute top-3 left-27
-           border-1 cursor-pointer  border-green-600  bg-white shadow-2xl rounded-full text-green-600 text-sm"
-          style={{ fontSize: "18px" }}
-        >
-          edit
-        </span>
         <h2 className="text-lg font-semibold text-gray-800 mb-2">
           Verifications
         </h2>
-        {profileData.verifications.length > 0 ? (
-          <ul className="space-y-1 text-gray-700">
-            {profileData.verifications.map((verification, index) => (
-              <li key={index}>{verification}</li>
-            ))}
-          </ul>
-        ) : (
-          <p className="text-gray-600">No verifications available.</p>
-        )}
+        <p className="text-green-400"> {isVerified}</p>
       </div>
 
       {/* Licenses */}
       <div className="mb-6">
         <h2 className="text-lg font-semibold text-gray-800 mb-2">Licenses</h2>
-        {profileData.licenses.length > 0 ? (
-          <ul className="space-y-1 text-gray-700">
-            {profileData.licenses.map((license, index) => (
-              <li key={index}>{license}</li>
-            ))}
-          </ul>
+        {Licenses > 0 ? (
+          <p>{Licenses}</p>
         ) : (
           <p className="text-gray-600">No licenses available.</p>
         )}
       </div>
 
       {/* Education */}
-      <Education />
+      <p className="text-gray-600">No licenses available.</p>
+      <Education Education={education} />
     </div>
   );
 };
