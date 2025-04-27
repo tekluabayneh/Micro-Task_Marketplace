1f6845a241cdd30fd28d3a4037a58c338029d7fc
teklu abayneh <tekluabayneh@gmail.com>
2025-04-27 15:49:48 +0300

    Update store configuration to manage freelancer profile state

--- client/src/components/store/store.js
+++ client/src/components/store/store.js
@@ -1,13 +1,15 @@
 import clientProfileSettingSlice from "../Slices/clientProfileSettingSlice";
 import clientSearchSlice from "../Slices/clientSearchSlice";
 import freelancerSearchSlice from "../Slices/freelancerSearchSlice";
+import FreelancerProfileSettingSlice from "../Slices/FreelancerProfileSettingSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
-    freelancerSearchSlice: freelancerSearchSlice,
-    clientSearchSlice: clientSearchSlice,
     clientProfileSettingSlice: clientProfileSettingSlice,
+    clientSearchSlice: clientSearchSlice,
+    FreelancerProfileSettingSlice: FreelancerProfileSettingSlice,
+    freelancerSearchSlice: freelancerSearchSlice,
   },
 });
 
