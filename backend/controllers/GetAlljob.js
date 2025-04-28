632067c56815b9aa1701a43e15af2f953bd15930
teklu abayneh <tekluabayneh@gmail.com>
2025-04-28 20:24:01 +0300

    Add GetAlljob.js controller

---
+++ backend/controllers/GetAlljob.js
@@ -0,0 +1,24 @@
+const db = require("../config/db");
+
+const GetAllJobs = async (req, res) => {
+  try {
+    console.log("this is working man");
+
+    // Get the client ID based on their email
+    const GetallJobs = "SELECT * FROM jobs limit 50";
+    const [result] = await db.execute(GetallJobs);
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Client not found" });
+    }
+    return res.status(200).json({ success: true, response: result });
+  } catch (error) {
+    console.error("Error fetching jobs:", error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching jobs",
+      error: error.message,
+    });
+  }
+};
+module.exports = GetAllJobs;
