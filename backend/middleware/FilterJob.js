87ceedf580dba01aed19e844473c9e7edb357641
teklu abayneh <tekluabayneh@gmail.com>
2025-04-30 19:24:47 +0300

    Update job filter middleware

--- backend/middleware/FilterJob.js
+++ backend/middleware/FilterJob.js
@@ -2,11 +2,10 @@ const db = require("../config/db");
 
 const FilterJob = async (req, res, next) => {
   let { Search } = req.query;
-  console.log(Search);
   try {
     if (!Search) return res.status(400).json({ error: "Missing search query" });
+
     if (Search === "All") {
-      console.log(Search);
       const getallProfileQuery = "SELECT * FROM jobs LIMIT 50";
       const [FilterResult] = await db.execute(getallProfileQuery);
 
@@ -17,7 +16,7 @@ const FilterJob = async (req, res, next) => {
 
     let searchTerm = `%${Search}%`;
 
-    const query = `SELECT * FROM jobs WHERE jobTitle LIKE ? OR proposal LIKE ? OR budget LIKE ? OR experience LIKE ?`;
+    const query = `SELECT * FROM jobs WHERE jobTitle LIKE ? OR budget LIKE ? OR experience LIKE ? OR jobSize LIKE ?`;
 
     let data = [searchTerm, searchTerm, searchTerm, searchTerm];
 
