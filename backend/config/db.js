d4358ef0ad9fdd1613847b9635fa7706c778643e
teklu abayneh <tekluabayneh@gmail.com>
2025-05-03 19:03:39 +0300

    Fix database connection settings

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "root",
-  database: "microworkdb",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
