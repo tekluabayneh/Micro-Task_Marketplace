5881fb6af04be40325d4bbd1919467d86b71856c
teklu abayneh <tekluabayneh@gmail.com>
2025-05-04 15:52:33 +0300

    Update MyPostedProjects page layout

--- client/src/pages/Jobs/MYPostedProjects.jsx
+++ client/src/pages/Jobs/MYPostedProjects.jsx
@@ -8,7 +8,7 @@ const MyJobs = () => {
   );
 
   return (
-    <div className="min-h-screen bg-gray-50 py-10 px-4">
+    <div className="min-h-screen bg-gray-50 py-10 px-4 mt-12">
       <div className="max-w-5xl mx-auto">
         <h2 className="text-3xl font-bold text-gray-800 mb-8 text-center">
           My Posted Jobs
