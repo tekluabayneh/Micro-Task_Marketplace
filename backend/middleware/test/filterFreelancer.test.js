d2853da13fb91e21c265affb622e7b2391428c1e
teklu abayneh <tekluabayneh@gmail.com>
2025-05-21 23:25:50 +0300

    add the second test case for filterFreelancer profile middleware

--- backend/middleware/test/filterFreelancer.test.js
+++ backend/middleware/test/filterFreelancer.test.js
@@ -11,19 +11,44 @@ let req, res, next;
 
 beforeEach(() =>{
 
-   req ={query:{ Search:{} }
+   req ={query:{ Search:{} };
 
    res = {
      status:jest.fn().mockReturnThis(),
      json:jest.fn(),
+    next = jest.fn(),
+   };
 
-   }
+
+test('should return 400 if the Seach query is empty',
+  async ()=> {
+
+await filterfreelancer(req, res, next)
+
+expect(req.query.Search).toHaveBeenCalledwith(400)
+
+expect(res.status).toHaveBeenCalledwith({error:'missing search query'})
+
+ // since there is Erroe the next shouldn't be called
+ expect(next).not.toHaveBeenCalled()
 
 
+  }
 
-test('should return 400 if the Seach query is empty', ()=>{
-  })
+test('should fetch all the profile if the query is not All', async () =>{
 
+req.query.Search = "Frontend Developer"
+const mockResult =  [{id:1, name:'teklu'}]
+
+  // and we have to also mock the database result 
+  db.execute.mockResolvedValueOnce([mockResult])
+
+
+filterfreelancer(req,res, next)
+
+expect(req.SearchResult).toEqual(mockResult)
+  expect(req.)
+}
 
 
 })
