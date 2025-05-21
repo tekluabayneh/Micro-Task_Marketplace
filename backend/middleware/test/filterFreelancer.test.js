7610b4d5b7580376fe684b091ac6720986941790
teklu abayneh <tekluabayneh@gmail.com>
2025-05-21 23:53:47 +0300

    adding the final step case for filterFreelancer profile test

--- backend/middleware/test/filterFreelancer.test.js
+++ backend/middleware/test/filterFreelancer.test.js
@@ -1,78 +1,51 @@
-const filterfreelancer = require("../filterFreelancer.js")
+const filterfreelancer = require("../filterFreelancer.js");
 
+const filterfreelancer = require("../filterFreelancer.js");
+jest.mock("filterfreelancer.js");
+descibe("filterfreelancer middlware", () => {
+  let req, res, next;
 
+  beforeEach(() => {
+    req = { query: {} };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    next = jest.fn();
+  });
 
-const filterfreelancer = require("../filterFreelancer.js")
-jest.mock('filterfreelancer.js')
-descibe('filterfreelancer middlware', ()=>{
+  test("should return 400 if the Seach query is empty", async () => {
+    await filterfreelancer(req, res, next);
 
-let req, res, next;
+    expect(req.query.Search).toHaveBeenCalledwith(400);
 
+    expect(res.status).toHaveBeenCalledwith({ error: "missing search query" });
 
-beforeEach(() =>{
+    // since there is Erroe the next shouldn't be called
+    expect(next).not.toHaveBeenCalled();
+  });
 
-   req ={query:{ Search:{} };
+  test("should fetch all the profile if the query is not All", async () => {
+    req.query.Search = "Frontend Developer";
+    const mockResult = [{ id: 1, name: "teklu" }];
 
-   res = {
-     status:jest.fn().mockReturnThis(),
-     json:jest.fn(),
-    next = jest.fn(),
-   };
+    // and we have to also mock the database result
+    db.execute.mockResolvedValueOnce([mockResult]);
 
+    filterfreelancer(req, res, next);
 
-test('should return 400 if the Seach query is empty',
-  async ()=> {
+    expect(req.SearchResult).toEqual(mockResult);
+    expect(db.execute).toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
 
-await filterfreelancer(req, res, next)
+  test("should retun internal server error", async () => {
+    req.query.search = "All";
+    db.execute.mockResolvedValueOnce(new Error("Db Error"));
 
-expect(req.query.Search).toHaveBeenCalledwith(400)
+    expect(res.status).toHaveBeenCalledwith(400);
+    expect(res.json).toHaveBeenCalledwith({ error: "Something went Wrong" });
 
-expect(res.status).toHaveBeenCalledwith({error:'missing search query'})
-
- // since there is Erroe the next shouldn't be called
- expect(next).not.toHaveBeenCalled()
-
-
-  }
-
-test('should fetch all the profile if the query is not All', async () =>{
-
-req.query.Search = "Frontend Developer"
-const mockResult =  [{id:1, name:'teklu'}]
-
-  // and we have to also mock the database result 
-  db.execute.mockResolvedValueOnce([mockResult])
-
-
-filterfreelancer(req,res, next)
-
-expect(req.SearchResult).toEqual(mockResult)
-  expect(db.execute).toHaveBeenCalled()
-  expect(next).toHaveBeenCalled()
-}
-
-
-})
-
-
-
-
-
-test('should retun internal server error' , async () =>{
-
-  res = {
-  status:jest.fn().mockReturnThis(),
-  json:jest.fn(),
-
-  }
-expect(res.status).toHaveBeenCalledwith({error:'Somthing went Wrong'})
-
-expect(res.json).toHaveBeenCalled()
-
-
-
-
-} 
-
-
-})
+    expect(next).not.toHaveBeenCalled();
+  });
+});
