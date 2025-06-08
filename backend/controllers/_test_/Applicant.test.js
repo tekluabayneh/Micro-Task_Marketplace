224438e09ad91885c184065861be40e878d7af9f
teklu abayneh <tekluabayneh@gmail.com>
2025-06-08 17:23:31 +0300

    Fix Applicant test mocks for user not found case

--- backend/controllers/_test_/Applicant.test.js
+++ backend/controllers/_test_/Applicant.test.js
@@ -1,22 +1,28 @@
-const { execute } = require("../../config/db")
+const execute = require("../../config/db.js")
 const Applicant = require("../Applicant.js")
+const db = require("../../config/db.js")
+const request = require("supertest")
 
-jest.mock("../../config/db.js", () => {
+
+
+jest.mock("../../config/db.js", () => ({
     execute: jest.fn()
-})
+}))
 
 const res = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn()
 }
 const req = {
-    email: "ex@gmail.com",
-    attachment_url: "one.com",
-    client_id: "1", job_id: "1",
-    cover_letter: "this is mcok cover letter"
+    body: {
+        email: "ex@gmail.com",
+        attachment_url: "one.com",
+        client_id: "1", job_id: "1",
+        cover_letter: "this is mcok cover letter"
+    }
 }
-const next = jest.fn()
 
+const next = jest.fn()
 
 beforeEach(() => {
     jest.clearAllMocks()
@@ -43,20 +49,23 @@ describe("Applicant test", () => {
     })
 
     it('should return 400 if the user is not found ', async () => {
-        execute.jest.mockResolvedValue([])
+
+        db.execute.mockResolvedValue([])
+
         await Applicant(req, res, next)
 
+        expect(db.execute).toHaveBeenCalled()
 
-
-
+        expect(next).not.toHaveBeenCalled()
     })
 
 
 
+    it('should send and call the next if it pass ', async () => {
 
+        await Applicant(req, res, next)
 
-
-
-
+        expect(next).toHaveBeenCalled()
+    })
 
 })
