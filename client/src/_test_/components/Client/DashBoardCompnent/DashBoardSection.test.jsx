2ae774e9b27e392fba3eead3dbdf94f1b5fdaf70
teklu abayneh <tekluabayneh@gmail.com>
2025-06-08 18:08:27 +0300

    Update dashboard component test for client

--- client/src/_test_/components/Client/DashBoardCompnent/DashBoardSection.test.jsx
+++ client/src/_test_/components/Client/DashBoardCompnent/DashBoardSection.test.jsx
@@ -1,8 +1,10 @@
 import DashBaordSection from "../../../../components/Client/DashBoardComponent/DashboardSection.jsx"
+import { render, screen } from '@testing-library/react'
+import {it, describe } from "vitest"
 
-describe(" DashboardSection test", () => {
- 
-
-
-
+describe('App component', () => {
+  it('renders a welcome message', () => {
+    render(<DashBaordSection/>)
+  })
 })
+
