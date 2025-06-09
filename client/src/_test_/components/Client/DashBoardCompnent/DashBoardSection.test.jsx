f5ad52a27d93effbcaba68ba49a7dbcd514d17b5
teklu abayneh <tekluabayneh@gmail.com>
2025-06-09 19:00:37 +0300

    Fix DashboardSection test with proper jest-dom matcher

--- client/src/_test_/components/Client/DashBoardCompnent/DashBoardSection.test.jsx
+++ client/src/_test_/components/Client/DashBoardCompnent/DashBoardSection.test.jsx
@@ -1,10 +1,32 @@
 import DashBaordSection from "../../../../components/Client/DashBoardComponent/DashboardSection.jsx"
 import { render, screen } from '@testing-library/react'
-import {it, describe } from "vitest"
+import React   from "react"
+import {it, describe, expect } from "vitest"
 
-describe('App component', () => {
-  it('renders a welcome message', () => {
-    render(<DashBaordSection/>)
+describe('testing if testing is working ', () => {
+  it('just test ', () => {
+  let props = {title:"we loave Testing",rightContent:"rightContent",Children:"Children",ClassName:"ClassName"}
+    render(<DashBaordSection {...props} />)
+
+
+   const headingElement = screen.getByRole("heading") 
+   expect(headingElement).toBeInTheDocument()
+   expect(headingElement).toHaveTextContent("we loave Testing")
+
+	  const  getElementCalss  = screen.getByTestId("dash-class")
+	   expect(getElementCalss).toBeInTheDocument()
+	  expect(getElementCalss).toHaveClass("mb-8")
+
+
+		const getRgitElement = screen.getByTestId("right-testid")
+		expect(getRgitElement).toBeInTheDocument()
+		expect(getRgitElement).toHaveTextContent("rightContent")
+
+
+
+
+
   })
+
 })
 
