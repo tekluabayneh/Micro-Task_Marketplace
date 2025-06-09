f056d006b69f502489ad56243a60d58dc30c6a6c
teklu abayneh <tekluabayneh@gmail.com>
2025-06-09 19:00:37 +0300

    Update DashboardSection component content and structure

--- client/src/components/Client/DashBoardComponent/DashboardSection.jsx
+++ client/src/components/Client/DashBoardComponent/DashboardSection.jsx
@@ -3,12 +3,12 @@ import { cn } from "./utils";
 
 const DashboardSection = ({ title, rightContent, children, className }) => {
   return (
-    <div className={cn("mb-8", className)}>
+    <div data-testid="dash-class" className={cn("mb-8", className)}>
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-4">
         <div>
           <h2 className="text-xl font-bold text-gray-900">{title}</h2>
         </div>
-        {rightContent && <div className="mt-2 md:mt-0">{rightContent}</div>}
+        {rightContent && <div  data-testid="right-testid" className="mt-2 md:mt-0">{rightContent}</div>}
       </div>
       {children}
     </div>
