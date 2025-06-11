593fe83f4a0a73d0376b50f9da124254043efc2d
teklu abayneh <tekluabayneh@gmail.com>
2025-06-11 16:58:54 +0300

    Update DashboardTabs test for improved coverage or logic

--- client/src/_test_/components/Client/DashBoardCompnent/DashboardTabs.test.jsx
+++ client/src/_test_/components/Client/DashBoardCompnent/DashboardTabs.test.jsx
@@ -1,19 +1,21 @@
 
-import { describe, it } from "vitest";
 import DashboardTabs from "../../../../components/Client/DashBoardComponent/DashboardTabs";
 import { screen, render } from "@testing-library/react";
 import axios from "axios";
+import {it, vi, describe, expect, beforEach} from "vitest"
+import MockAddapter from "axios-mock-adapter"
 
+
+const mcok = new MockAddapter(axios)
 
 describe("DashboadTabs test", () => {
     const tabs = {
         tab1: "this is tab one ", tab2: "this is tab tow"
     }
+
     it('it should fetch the client profile data', async () => {
         render(
             <DashboardTabs {...tabs} />)
-
-
 
     })
 
