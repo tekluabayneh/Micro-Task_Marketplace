ef7176445101fb66763f4ffa8a460eac735ec96f
teklu abayneh <tekluabayneh@gmail.com>
2025-06-12 17:34:30 +0300

    ✅ Add unit tests for ApplicantPortfolio controller

--- backend/controllers/_test_/ApplicantPortfolio.test.js
+++ backend/controllers/_test_/ApplicantPortfolio.test.js
@@ -15,7 +15,6 @@ const res = {
     json: jest.fn(),
 }
 
-
 describe("ApplicantPortFolio test", () => {
 
     it('should return if the user email is not provided', async () => {
@@ -26,7 +25,8 @@ describe("ApplicantPortFolio test", () => {
     })
 
 
-    it('should return 404 if the freelaner not fould ', async () => {
+    it('should  return 404 cline is not found ', async () => {
+        db.execute.mockReturnValueOnce(Promise.resolve([[], []]))
 
         const req = {
             query: {
@@ -34,11 +34,69 @@ describe("ApplicantPortFolio test", () => {
             }
         }
 
-        db.execute.mockReturnValueOnce([])
-
         await ApplicantPortFolio(req, res)
         expect(db.execute).toHaveBeenCalledWith(expect.stringContaining("SELECT users.id AS freelancer_id"), expect.any(Array))
-
+        expect(res.status).toHaveBeenCalledWith(404)
+        expect(res.json).toHaveBeenCalledWith({ message: "Freelancer not found with the provided email." })
     })
+
+
+    it('should return 404 if the portfolio is not found', async () => {
+        db.execute
+            .mockReturnValueOnce(Promise.resolve([[{ freelancer_id: 1 }], []]))
+            .mockReturnValueOnce(Promise.resolve([[], []]));
+
+
+        const req = {
+            query: {
+                userEmail: "ex@email.com"
+            }
+        }
+
+
+        await ApplicantPortFolio(req, res)
+
+        expect(db.execute).toHaveBeenCalledWith(expect.stringContaining("SELECT * FROM portfolio WHERE freelancer_id = ?"), expect.any(Array));
+
+        expect(res.status).toHaveBeenCalledWith(404)
+        expect(res.json).toHaveBeenCalledWith({ message: "No portfolio found for the given email." })
+    })
+
+    it('should respond with 200 message ', async () => {
+        db.execute
+            .mockReturnValueOnce(Promise.resolve([[{ freelancer_id: 1 }], []]))
+            .mockReturnValueOnce(Promise.resolve([
+                [
+                    { id: 101, title: "My Portfolio", description: "React, Node.js" },
+                    { id: 102, title: "My Second Portfolio", description: "Vue, Laravel" }
+                ],
+                []
+            ]))
+
+        const req = {
+            query: {
+                userEmail: "ex@email.com"
+            }
+        }
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        }
+
+        await ApplicantPortFolio(req, res)
+
+        expect(db.execute).toHaveBeenCalledWith(expect.stringContaining("SELECT * FROM portfolio WHERE freelancer_id = ?"), expect.any(Array));
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith({
+            message: "Portfolio fetched successfully.",
+            portfolio: [
+                { id: 101, title: "My Portfolio", description: "React, Node.js" },
+                { id: 102, title: "My Second Portfolio", description: "Vue, Laravel" }
+            ]
+        })
+    })
+
+
 
 })
