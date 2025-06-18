e40e3673cbeec0c187fb7287018d0443ebafb161
teklu abayneh <tekluabayneh@gmail.com>
2025-06-18 13:56:33 +0300

    Update FetchJobs controller logic

--- backend/controllers/FetchJobs.js
+++ backend/controllers/FetchJobs.js
@@ -1,19 +1,19 @@
 const db = require("../config/db"); // Your MySQL connection
 
 const fetchJobController = async (req, res) => {
-  try {
-    const query = "SELECT * FROM jobs";
-    let [results] = await db.execute(query);
+    try {
+        const query = "SELECT * FROM jobs";
+        let [results] = await db.execute(query);
 
-    if (results.length === 0) {
-      return res.status(404).json({ message: "Job not found" });
+        if (results.length === 0) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+
+        res.status(200).json(results);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
     }
-
-    res.status(200).json(results);
-  } catch (error) {
-    console.log(err);
-    res.status(5000).json({ message: "Something went wrong" });
-  }
 };
 
 module.exports = fetchJobController;
