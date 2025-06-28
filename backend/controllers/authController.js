a6e7b176e31553c3ff3b93931cf7086be4f376cd
teklu abayneh <tekluabayneh@gmail.com>
2025-06-28 16:23:47 +0300

    Enhance: improved error handling in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,38 +1,39 @@
 const bcrypt = require("bcryptjs");
 const db = require("../config/db");
+
 const Register = async (req, res) => {
-  const { password, lastName, email, role, firstName } = req.body;
-  try {
-    let Gensalt = await bcrypt.genSalt(10);
-    let HashedPassword = await bcrypt.hash(password, Gensalt);
+    const { password, lastName, email, role, firstName } = req.body;
+    try {
+        let Gensalt = await bcrypt.genSalt(10);
+        let HashedPassword = await bcrypt.hash(password, Gensalt);
 
-    let data = [firstName, lastName, email, HashedPassword, role];
-    let insertUser =
-      "INSERT INTO users (firstName, lastName, email, Password, role)VALUES(?,?,?,?,?)";
-    let [result] = await db.execute(insertUser, data);
+        let data = [firstName, lastName, email, HashedPassword, role];
+        let insertUser =
+            "INSERT INTO users (firstName, lastName, email, Password, role)VALUES(?,?,?,?,?)";
+        let [result] = await db.execute(insertUser, data);
 
-    if (result.affectedRows > 0) {
-      return res.status(200).json({ message: "Registration successful!" });
-    } else {
-      return res
-        .status(500)
-        .json({ message: "Something went wrong, please try again." });
+        if (result.affectedRows > 0) {
+            return res.status(200).json({ message: "Registration successful!" });
+        } else {
+            return res
+                .status(500)
+                .json({ message: "Something went wrong, please try again." });
+        }
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ message: "Server error, please try again later." });
     }
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ message: "Server error, please try again later." });
-  }
 };
 const Login = (req, res) => {
-  const userInfoFromDB = req.userInfoFromDB;
-  // heck the user type and navigate them to the dashboard
-  if (userInfoFromDB[0].role === "freelancer") {
-    return res.status(200).json({ role: "Freelancer/Dashboard" });
-  } else {
-    return res.status(200).json({ role: "Client/Dashboard" });
-  }
+    const userInfoFromDB = req.userInfoFromDB;
+    // heck the user type and navigate them to the dashboard
+    if (userInfoFromDB[0].role === "freelancer") {
+        return res.status(200).json({ role: "Freelancer/Dashboard" });
+    } else {
+        return res.status(200).json({ role: "Client/Dashboard" });
+    }
 };
 
 module.exports = { Login, Register };
