ad3e07cca59715cbd586736443ffd835289a0973
teklu abayneh <tekluabayneh@gmail.com>
2025-06-28 16:23:46 +0300

    Refactor: updated job posting logic in JobPost controller

--- backend/controllers/JobPost.js
+++ backend/controllers/JobPost.js
@@ -1,5 +1,5 @@
 const JobPost = (req, res) => {
-  return res.status(201).json({ message: "Job posted successfully" });
+    return res.status(201).json({ message: "Job posted successfully" });
 };
 
 module.exports = JobPost;
